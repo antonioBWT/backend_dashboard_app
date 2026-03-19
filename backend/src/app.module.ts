8c1cdb5c69d9eab98366ff451a1cd31a232da0dd
Anton <antonio300dragon@gmail.com>
2026-03-19 14:52:39 +0100

    fix: api monolit front

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -89,7 +89,7 @@ import * as fs from 'fs';
 
     ServeStaticModule.forRoot({
       rootPath: path.join(process.cwd(), 'frontend', 'dist'),
-      exclude: ['/api/(.*)'],
+      exclude: ['/api*'],
     }),
     SettingsModule,
     AuthModule,
